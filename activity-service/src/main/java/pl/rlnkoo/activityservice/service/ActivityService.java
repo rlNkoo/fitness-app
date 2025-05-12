2304b5640d3a79973e649cdd9f776945859191ca
Artur Rolak <rolakartur96@gmail.com>
2025-05-12 15:56:37 +0200

    RabbitMQ added

--- activity-service/src/main/java/pl/rlnkoo/activityservice/service/ActivityService.java
+++ activity-service/src/main/java/pl/rlnkoo/activityservice/service/ActivityService.java
@@ -1,6 +1,9 @@
 package pl.rlnkoo.activityservice.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import pl.rlnkoo.activityservice.dto.ActivityRequest;
 import pl.rlnkoo.activityservice.dto.ActivityResponse;
@@ -12,11 +15,20 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ActivityService {
 
     private final ActivityRepository activityRepository;
 
     private final UserValidationService userValidationService;
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
 
     public ActivityResponse trackActivity(ActivityRequest request) {
 
@@ -34,6 +46,13 @@ public class ActivityService {
                 .build();
 
         Activity savedActivity = activityRepository.save(activity);
+
+        try {
+            rabbitTemplate.convertAndSend(exchange, routingKey, savedActivity);
+        } catch (Exception e) {
+            log.error("Failed to publish activity to RabbitMQ: ", e);
+        }
+
         return mapToResponse(savedActivity);
     }
 
